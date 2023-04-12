830246332a6f5a62715f2fbe3049890644142fd8
ale <alexcs9230@gmail.com>
2023-04-12 15:08:27 +0200

    started navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./../../global.css";
+import "./Navbar.css";
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <nav className="navbar">
+        <h1 className="logo">
+          <span>E</span>VENTIA
+        </h1>
+        <div className="logup">
+          <h1 className="login">Login</h1>
+          <button className="singup">Sing Up</button>
+        </div>
+      </nav>
+    </div>
+  );
 }
