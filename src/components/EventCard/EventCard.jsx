0649be1abb64bb187d54651e9ec556fea047f187
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-04-13 12:05:33 +0200

    create card event with styles css

---
+++ src/components/EventCard/EventCard.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import styles from './eventCard.module.css';
+
+export default function EventCard() {
+  return (
+    <>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.eventCardContainer}>
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.imgCardEvent}
+            src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+            alt='img event'
+          />
+          <div className={styles.bottomEventCard}>
+            <div className={styles.dateContainer}>
+              <h2>Fecha</h2>
+            </div>
+            <div className={styles.infoContainer}>
+              <h2 className={styles.titleEvent}>Title of the event</h2>
+              <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
+              <button>More</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
