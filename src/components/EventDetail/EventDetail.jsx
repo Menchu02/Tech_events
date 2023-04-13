899dd399e02c983e5dd98451bed7f9316c5893ac
ale <alexcs9230@gmail.com>
2023-04-13 16:01:31 +0200

    added text and buttns to eventDetail

--- src/components/EventDetail/EventDetail.jsx
+++ src/components/EventDetail/EventDetail.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "../Slider/Slider";
 import styles from "./EventDetail.module.css";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import { BsFillPersonFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 function EventDetail() {
@@ -11,7 +12,7 @@ function EventDetail() {
   return (
     <div>
       <div className={styles.container}>
-        <img className={styles.img} src={img} alt="image" />
+        <img className={styles.eventimg} src={img} alt="image" />
         <div className={styles.text}>
           <h1 className={styles.title}>Title of event</h1>
           <h1 className={styles.by}>By Name</h1>
@@ -24,6 +25,23 @@ function EventDetail() {
           </Link>
         </div>
       </div>
+      <div className={styles.containerbutton}>
+        <button className={styles.joinButton}>
+          <BsFillPersonFill /> 0/10
+        </button>
+      </div>
+      <h1 className={styles.titles}>Description</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        tempora impedit ex officiis iure consequuntur nisi minus iusto voluptate
+        exercitationem voluptates esse, reiciendis iste deserunt? Praesentium
+        ullam nam ipsa at?
+      </p>
+      <h1 className={styles.titles}>Date & Time</h1>
+      <p>Day, Month day, year at hour </p>
+      <Link>
+        <h1 className={styles.links}> Add to calendar </h1>
+      </Link>
     </div>
   );
 }
