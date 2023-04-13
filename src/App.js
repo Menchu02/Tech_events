9d7867dd43c5ad26da6077fa316056757f978ec2
ale <alexcs9230@gmail.com>
2023-04-13 11:44:09 +0200

    finished navbar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
-import './App.css';
-import Navbar from './components/NavBar/NavBar';
-import Slider from './components/Slider/Slider';
-import SerchBar from './components/SearchBar/SearchBar';
-import Highlight_events from './components/Highlight_events/Highlight_events';
-import UpComing_events from './components/UpComing_events/UpComing_events';
-import Past_events from './components/Past_events/Past_events';
-import Footer from './components/Footer/Footer';
+import "./App.css";
+import Navbar from "./components/NavBar/NavBar";
+import Slider from "./components/Slider/Slider";
+import SerchBar from "./components/SearchBar/SearchBar";
+import Highlight_events from "./components/Highlight_events/Highlight_events";
+import UpComing_events from "./components/UpComing_events/UpComing_events";
+import Past_events from "./components/Past_events/Past_events";
+import Footer from "./components/Footer/Footer";
+import { BrowserRouter } from "react-router-dom";
 // import imgSlider from '../../img';
 
 function App() {
   const imgSlider = [
-    'https://cnnespanol.cnn.com/wp-content/uploads/2022/02/jardineria-consejos-principiantes.jpg?quality=100&strip=info',
-    'https://media.thebostoncalendar.com/images/q_auto,fl_lossy/v1653592932/zxjdayzo1zkmo1ewzgjf/outdoor-yoga-in-cambridge--36.jpg',
-    'https://isdicoders.com/wp-content/uploads/2022/03/daw-dam-asir-asix-o-hacer-un-coding-bootcamp-1-scaled.jpeg',
+    "https://cnnespanol.cnn.com/wp-content/uploads/2022/02/jardineria-consejos-principiantes.jpg?quality=100&strip=info",
+    "https://media.thebostoncalendar.com/images/q_auto,fl_lossy/v1653592932/zxjdayzo1zkmo1ewzgjf/outdoor-yoga-in-cambridge--36.jpg",
+    "https://isdicoders.com/wp-content/uploads/2022/03/daw-dam-asir-asix-o-hacer-un-coding-bootcamp-1-scaled.jpeg",
   ];
   return (
-    <div className='App'>
-      <Navbar />
-      <Slider images={imgSlider} />
-      <SerchBar />
-      <Highlight_events />
-      <UpComing_events />
-      <Past_events />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Slider images={imgSlider} />
+        <SerchBar />
+        <Highlight_events />
+        <UpComing_events />
+        <Past_events />
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
 import "./../../App.css";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsInstagram, BsTwitter, BsLinkedin } from "react-icons/bs";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
       <nav className="navbar_desk">
-        <h1 className="logo">
-          <span>E</span>VENTIA
-        </h1>
+        <h1 className="logo">EVENTIA</h1>
         <div className="logup">
           <h1 className="login">Login</h1>
           <button className="singup">Sing Up</button>
@@ -18,7 +19,30 @@ export default function NavBar() {
       <div className="navbar">
         <h1 className="nav_logo">EVENTIA</h1>
         <div className={`nav_items ${isOpen && "open"}`}>
-          <h1>test</h1>
+          <Link>
+            <h1>Active Events</h1>
+          </Link>
+          <Link>
+            <h1>Past Events</h1>
+          </Link>
+          <Link>
+            <h1>About Us</h1>
+          </Link>
+          <h1 className="media">Social Media</h1>
+          <div className="socialmedia">
+            <Link>
+              <FaFacebookSquare></FaFacebookSquare>
+            </Link>
+            <Link>
+              <BsInstagram></BsInstagram>
+            </Link>
+            <Link>
+              <BsTwitter></BsTwitter>
+            </Link>
+            <Link>
+              <BsLinkedin></BsLinkedin>
+            </Link>
+          </div>
         </div>
         <div
           className={`nav_toggle ${isOpen && "open"}`}
