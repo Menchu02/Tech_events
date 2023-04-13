6dd937c0b7399d6d3fa83f0391f0de38061cbda1
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-04-13 16:14:10 +0200

    added eventsServices

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,125 +1,27 @@
 import React from 'react';
 import styles from './eventCard.module.css';
 
-export default function EventCard() {
+export default function EventCard({ item }) {
   return (
-    <>
-      <div className={styles.patherContainer}>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
+    <div className={styles.eventCardContainer}>
+      <div className={styles.imgContainer}>
+        <img
+          className={styles.imgCardEvent}
+          src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
+          alt='img event'
+        />
+      </div>
+      <div className={styles.bottomEventCard}>
+        <div className={styles.dateContainer}>
+          <h2>{item.eventDate}</h2>
         </div>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles.eventCardContainer}>
-          <div className={styles.imgContainer}>
-            <img
-              className={styles.imgCardEvent}
-              src='https://codersera.com/blog/wp-content/uploads/2019/07/BLOG-23-L-3.jpg'
-              alt='img event'
-            />
-            <div className={styles.bottomEventCard}>
-              <div className={styles.dateContainer}>
-                <h2>Fecha</h2>
-              </div>
-              <div className={styles.infoContainer}>
-                <h2 className={styles.titleEvent}>Title of the event</h2>
-                <p className={styles.locationEvent}>Location 9:00pm-12:00pm</p>
-                <button>More</button>
-              </div>
-            </div>
-          </div>
+        <div className={styles.infoContainer}>
+          <h2 className={styles.titleEvent}>{item.name}</h2>
+          <p className={styles.locationEvent}>{item.location}</p>
+          <p className={styles.locationEvent}>{item.eventHour}</p>
+          <button>More</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/components/UpComing_events/UpComing_events.jsx
+++ src/components/UpComing_events/UpComing_events.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import EventCard from '../EventCard/EventCard';
 import styles from './upComingEvents.module.css';
-
+import eventiaServices from '../../apiService/eventsServices';
 export default function UpComing_events() {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    eventiaServices.getAllEvents().then((data) => setEvents(data));
+  }, []);
+
   return (
     <div className={styles.galleryContainer}>
-      <EventCard />
+      {events.map((item) => (
+        <EventCard key={item.id} item={item} />
+      ))}
+      {/* <EventCard /> */}
     </div>
   );
 }
