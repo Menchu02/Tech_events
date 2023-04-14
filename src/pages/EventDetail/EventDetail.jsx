6e4f0cfc14bfdce6061374ae70348d7a4d6958c1
ale <alexcs9230@gmail.com>
2023-04-14 13:35:41 +0200

    styled eventDetail in web version

--- src/pages/EventDetail/EventDetail.jsx
+++ src/pages/EventDetail/EventDetail.jsx
@@ -7,9 +7,9 @@ import { Link, useParams } from "react-router-dom";
 import eventiaServices from "../../apiService/eventsServices";
 
 function EventDetail() {
-  const img = [
-    "https://images.pexels.com/photos/7776215/pexels-photo-7776215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  ];
+  // const img = [
+  //   "https://images.pexels.com/photos/7776215/pexels-photo-7776215.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  // ];
 
   const { id } = useParams();
   const [eventDetail, setEventDetail] = useState({});
@@ -34,21 +34,33 @@ function EventDetail() {
             </h1>
           </Link>
         </div>
+        <div className={styles.deskJoin}>
+          <h1>Date & Time</h1>
+          <h2>{eventDetail.eventDate}</h2>
+          <h3>Add To Calendar</h3>
+          <button className={styles.deskJoinButton}>
+            {" "}
+            <BsFillPersonFill /> 0/10
+          </button>
+          <button className={styles.deskShareButton}>Share with friends</button>
+        </div>
       </div>
       <div className={styles.containerbutton}>
         <button className={styles.joinButton}>
           <BsFillPersonFill /> 0/10
         </button>
       </div>
-      <h1 className={styles.titles}>Description</h1>
-      <p>{eventDetail.description}</p>
-      <h1 className={styles.titles}>Date & Time</h1>
-      <p>
-        {eventDetail.eventDate} at {eventDetail.eventHour}
-      </p>
-      <Link>
-        <h1 className={styles.links}> Add to calendar </h1>
-      </Link>
+      <div className={styles.textContainer}>
+        <h1 className={styles.titles}>Description</h1>
+        <p>{eventDetail.description}</p>
+        <h1 className={styles.titles}>Date & Time</h1>
+        <p>
+          {eventDetail.eventDate} at {eventDetail.eventHour}
+        </p>
+        <Link>
+          <h1 className={styles.links}> Add to calendar </h1>
+        </Link>
+      </div>
     </div>
   );
 }
