70fea96099640370553584bb19ec636d0cdfe33a
Carmen Blanco Delgado <94396032+Menchu02@users.noreply.github.com>
2023-04-14 12:21:28 +0200

    added new styles  in gallery events

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,18 +3,22 @@ import styles from './slider.module.css';
 import { FaGreaterThan, FaLessThan } from 'react-icons/fa';
 
 export default function Slider({ images }) {
-  //variables y estados
+  // estado comienza en posicion 0
   const [img, setImg] = useState(0);
+
+  // variable con la longitud que contiene el array images
   const totalQuantity = images?.length;
 
   if (!Array.isArray(images) || totalQuantity === 0) {
     return;
   }
 
+  //icono suma
   const handlerNextImg = () => {
     setImg(img === totalQuantity - 1 ? 0 : img + 1);
   };
 
+  //icono resta
   const handlerPreviousImg = () => {
     setImg(img === 0 ? totalQuantity - 1 : img - 1);
   };
@@ -24,6 +28,8 @@ export default function Slider({ images }) {
         className={styles.sliderIconLess}
         onClick={handlerPreviousImg}
       />
+
+      {/* mapeo la prop images */}
       {images.map((item, index) => {
         return (
           <div key={index}>
