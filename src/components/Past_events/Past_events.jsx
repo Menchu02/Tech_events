89c072cac5ed39f3bd3eb94b2089850878fc7676
ifadoua2 <f647379@gmail.com>
2023-04-18 09:44:43 +0200

    add styles pastEvents

--- src/components/Past_events/Past_events.jsx
+++ src/components/Past_events/Past_events.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import eventiaServices from '../../apiService/eventsServices';
 import EventCard from '../EventCard/EventCard';
-
+import styles from "./pastEvents.module.css";
 export default function Past_events() {
   const [notAvailable, setNotAvailable] = useState([]);
 
@@ -12,9 +12,13 @@ export default function Past_events() {
   return (
     <div>
       <h1>Past events</h1>
+      <div className={styles.gallerycontainer}>
       {notAvailable.map((item) => (
         <EventCard item={item} key={item.id} />
       ))}
+
+      </div>
+       c
     </div>
   );
 }
