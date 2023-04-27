2c03573081ce2c1a62ad37926690fbc26c79d35f
marcygon <marciasofia49@gmail.com>
2023-04-27 14:53:57 +0200

    navdarTried

--- src/components/NavBar/Navbar.test.js
+++ src/components/NavBar/Navbar.test.js
@@ -1,11 +1,11 @@
-// import { render, screen } from "@testing-library/react";
-// import NavBar from "./NavBar";
-// import React from "react";
+import { render, screen } from "@testing-library/react";
+import NavBar from "./NavBar";
+import React from "react";
 
-// describe('Navbar in tdd', () => {
-//     beforeEach(() => render(<NavBar/>))
-//     test('render navbar component correctly', () => {
-//         screen.debug()
-//         expect(true).toBeTruthy()
-//     })
-// })
+describe('Navbar in tdd', () => {
+    beforeEach(() => render(<NavBar/>))
+    test('render navbar component correctly', () => {
+        screen.debug()
+        expect(true).toBeTruthy()
+    })
+})
